5c50c5d35f9fd2328dac973a007738f719ec99f8
Test User <test@example.com>
2026-03-29 19:05:51 +0300

    Fix widget project

--- TestApp/TestApp/TestAppApp.swift
+++ TestApp/TestApp/TestAppApp.swift
@@ -5,20 +5,6 @@ struct TestAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onOpenURL { url in
-                    handleURL(url)
-                }
-        }
-    }
-    
-    func handleURL(_ url: URL) {
-        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
-              let host = components.host else { return }
-        
-        if host == "select", let option = components.queryItems?.first(where: { $0.name == "option" })?.value,
-           let optionIndex = Int(option) {
-            UserDefaults(suiteName: "group.testapp")?.set(optionIndex, forKey: "selectedOption")
-            WidgetCenter.shared.reloadAllTimelines()
         }
     }
 }
